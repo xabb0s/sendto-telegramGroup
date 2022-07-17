6440cff85d2a2000285d0df2264e2e841d7bae07
xabb0s <abbosxudoyberganov2005@gmail.com>
2022-07-17 15:01:39 +0500

    fixed bugs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-  res.res("Hello World!")
+  res.send("Hello World!")
 })
 
 app.post("/fishes", (req, res) => {
